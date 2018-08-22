635730192587ef8d3dca9218b197b3cbf4dfe30c
bsmerbeck <bsmerbeck@gmail.com>
2018-08-22 19:37:19 -0400

    Initial Commit

---
+++ CleanArchitectureRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanArchitectureRealm
+//
+//  Created by Brenden Smerbeck on 8/22/18.
+//  Copyright © 2018 Brenden Smerbeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
